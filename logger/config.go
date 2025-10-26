4e30ce462fea78f9c58bdfdd86ba67b07890c48b
Sergey Tarakanov <jjtsksndeveloper@gmail.com>
2025-10-26 19:08:24 +0300

    feat(logger): add simple zerolog logger with console and loki writers

---
+++ logger/config.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"errors"
+
+	"github.com/rs/zerolog"
+)
+
+type Config struct {
+	LokiURL       string
+	LokiUser      string
+	LokiPass      string
+	LogLevel      zerolog.Level
+	TimeFormat    string
+	EnableLoki    bool
+	EnableConsole bool
+}
+
+func (c *Config) Validate() error {
+	if c.TimeFormat == "" {
+		return errors.New("time format is required")
+	}
+	if c.EnableLoki && c.LokiURL == "" {
+		return errors.New("loki url is required when loki is enabled")
+	}
+	return nil
+}
---
+++ logger/logger.go
@@ -0,0 +1,62 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/rs/zerolog"
+	"github.com/thisismz/zerolog-loki/client"
+)
+
+var (
+	once    sync.Once
+	logger  *zerolog.Logger
+	writers []io.Writer
+	mu      sync.Mutex
+)
+
+func New(cfg Config) (*zerolog.Logger, error) {
+	if err := cfg.Validate(); err != nil {
+		return nil, fmt.Errorf("invalid config: %w", err)
+	}
+
+	var err error
+	once.Do(func() {
+		writers = make([]io.Writer, 0)
+		if cfg.EnableConsole {
+			consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: cfg.TimeFormat}
+			writers = append(writers, consoleWriter)
+		}
+
+		if cfg.EnableLoki {
+			var lokiWriter io.Writer
+			lokiWriter, err = client.NewSimpleClient(cfg.LokiURL, cfg.LokiUser, cfg.LokiPass)
+			if err != nil {
+				return
+			}
+			writers = append(writers, lokiWriter)
+		}
+		multiWriter := zerolog.MultiLevelWriter(writers...)
+		lg := zerolog.New(multiWriter).Level(cfg.LogLevel).With().Timestamp().Logger()
+		logger = &lg
+
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("create loki client: %w", err)
+	}
+
+	return logger, nil
+}
+
+func Close() {
+	mu.Lock()
+	defer mu.Unlock()
+	for _, writer := range writers {
+		if closer, ok := writer.(io.Closer); ok {
+			_ = closer.Close()
+		}
+	}
+}
